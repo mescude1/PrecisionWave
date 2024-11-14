a713d15331234bfff87dfaffcb8c1dcf8b4a0fb4
Mauricio Escudero <mescude1@eafit.edu.co>
2024-11-14 04:53:37 +0000

    re-arrangin folder structure

--- backend/app/model/__init__.py
+++
@@ -1,8 +0,0 @@
-"""The model layer."""
-
-
-from .models import (User, Token)
-from .user_repository import UserRepository
-from .token_respository import TokenRepository
-
-__all__ = ["User", "TokenRepository", "Token", "UserRepository"]
