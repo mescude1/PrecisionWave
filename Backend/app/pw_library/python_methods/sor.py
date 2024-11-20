20f5db96434541dec917a3ff3b3d91d71e0a4747
Mauricio Escudero <ae_mescudero@contractor.indeed.com>
2024-11-19 21:57:13 -0500

    fix cast

--- Backend/app/pw_library/python_methods/sor.py
+++ Backend/app/pw_library/python_methods/sor.py
@@ -9,9 +9,9 @@ def sor(A, b, x0, w, tol=1e-10, max_iterations=1000):
         x_old = x.copy()
 
         for i in range(n):
-            s1 = sum(A[i, j] * x[j] for j in range(i))
-            s2 = sum(A[i, j] * x_old[j] for j in range(i + 1, n))
-            x[i] = (1 - w) * x_old[i] + w * (b[i] - s1 - s2) / A[i, i]
+            s1 = sum(A[i][j] * x[j] for j in range(i))
+            s2 = sum(A[i][j] * x_old[j] for j in range(i + 1, n))
+            x[i] = (1 - w) * x_old[i] + w * (b[i] - s1 - s2) / A[i][i]
 
         # Check for convergence
         if np.linalg.norm(x - x_old, ord=np.inf) < tol:
